ed04f95cecf6d26bb583a8d0491ae5033576177c
LeonardoValderas <l.v.bass@hotmail.com>
2025-04-23 22:49:21 -0300

    First commit

---
+++ Sources/DsPocPackage/Components/BottomText.swift
@@ -0,0 +1,23 @@
+//
+//  Untitled.swift
+//  dspoc
+//
+//  Created by Leonardo Valderas on 23/04/25.
+//
+
+import SwiftUI
+
+struct BottomText: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("BOTTOM TEXT")
+                .foregroundColor(Color.primary)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BottomText()
+}
---
+++ Sources/DsPocPackage/Components/TopText.swift
@@ -0,0 +1,23 @@
+//
+//  Untitled.swift
+//  dspoc
+//
+//  Created by Leonardo Valderas on 23/04/25.
+//
+
+import SwiftUI
+
+struct TopText: View {
+    var body: some View {
+        VStack {
+            Text("TOP TEXT")
+                .foregroundColor(Color.secondary)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TopText()
+}
